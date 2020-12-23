72d497544188175a06d43a70ba555b8d6dfb9cac
Telicia Watson <teli203@aol.com>
2020-12-22 22:17:13 -0600

    added folders teamprofile and sql w/schema

---
+++ sql/schema.sql
@@ -0,0 +1,10 @@
+-- Drops the teamprofile_db if it already exists --
+DROP DATABASE IF EXISTS teamprofile_db;
+
+-- Created the DB "teamprofile_db" (only works on local connections)
+CREATE DATABASE teamprofile_db;
+
+-- Use the DB teamprofile_db for all the rest of the script
+USE teamprofile_db;
+
+
